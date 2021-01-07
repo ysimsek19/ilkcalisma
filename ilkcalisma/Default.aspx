845395855a057ea23ea47086376920c6d7dcd94a
yalcinsimsek <ysimsek@karatekin.edu.tr>
2021-01-07 13:01:15 +0300

    Add project files.

---
+++ ilkcalisma/Default.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ilkcalisma.Default" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server"> 
+    <div class="ui list" id="list" runat="server"></div>
+</asp:Content>
---
+++ ilkcalisma/Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ilkcalisma
+{
+	public partial class Default : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			//Response.Write("yalçın şimşek");
+			//string[] liste = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+		Dictionary<string, string> liste = new Dictionary<string, string>();
+			liste.Add("Sun", "1.jpg");
+			liste.Add("Mon", "2.jpg");
+			liste.Add("Tue", "3.jpg");
+			liste.Add("Wed", "4.jpg");
+
+
+			liste.Add("Thu", "5.jpg");
+			liste.Add("Fri", "6.jpg");
+			liste.Add("Sat", "7.jpg");
+			//foreach (var aaaa in liste)
+			//{
+			//	list.InnerHtml += @"<li>"+aaaa+"</li>";
+
+			//}
+			//for (int i = 0; i < liste.Count(); i++)
+			foreach (var y in liste)
+			{
+			list.InnerHtml += @"<div class=""item""> <img class=""ui avatar image"" src="""+y.Value+@"""/><div class=""content""> <div class=""header""> "+y.Key+"</div></div></div>";
+				
+			}
+			//string yalcin = "KADIN";
+			//if (yalcin == "ERKEK")
+			//{ Response.Write("doğru"); }
+			//else
+			//{
+			//	Response.Write("yanlış");
+			//}
+			//int myNum;
+			//myNum = 15;
+			//string mesaj = "doğru";
+			////Response.Write(myNum);
+			//if (20 > myNum)
+
+			//	Response.Write(mesaj);
+
+
+			//else
+
+			//	Response.Write("Yanlış p");
+
+			////Response.Write((14 > myNum) ?"doğru" : "y");
+			//string txt = "<br/>We are the so-called \"Vikings\" from the north.";
+			//Response.Write(txt);
+
+
+
+		}
+
+
+		
+	}
+}
