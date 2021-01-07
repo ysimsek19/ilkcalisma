f864a645a91134a4063ba613d68c4fc47ee8a190
yalcinsimsek <ysimsek@karatekin.edu.tr>
2021-01-07 13:59:42 +0300

    ilk

--- ilkcalisma/Default.aspx.cs
+++ ilkcalisma/Default.aspx.cs
@@ -14,27 +14,25 @@ namespace ilkcalisma
 			//Response.Write("yalçın şimşek");
 			//string[] liste = { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
 
-		Dictionary<string, string> liste = new Dictionary<string, string>();
-			liste.Add("Sun", "1.jpg");
-			liste.Add("Mon", "2.jpg");
-			liste.Add("Tue", "3.jpg");
-			liste.Add("Wed", "4.jpg");
+			//Dictionary<string, string> liste = new Dictionary<string, string>();
+			//	liste.Add("Sun", "1.jpg");
+			//	liste.Add("Mon", "2.jpg");
+			//	liste.Add("Tue", "3.jpg");
+			//	liste.Add("Wed", "4.jpg");
+			//	liste.Add("Thu", "5.jpg");
+			//	liste.Add("Fri", "6.jpg");
+			//	liste.Add("Sat", "7.jpg");
+			//	//foreach (var aaaa in liste)
+			//	//{
+			//	//	list.InnerHtml += @"<li>"+aaaa+"</li>";
 
+			//	//}
+			//	//for (int i = 0; i < liste.Count(); i++)
+			//	foreach (var y in liste)
+			//	{
+			//	list.InnerHtml += @"<div class=""item""> <img class=""ui avatar image"" src="""+y.Value+@"""/><div class=""content""> <div class=""header""> "+y.Key+"</div></div></div>";
 
-			liste.Add("Thu", "5.jpg");
-			liste.Add("Fri", "6.jpg");
-			liste.Add("Sat", "7.jpg");
-			//foreach (var aaaa in liste)
-			//{
-			//	list.InnerHtml += @"<li>"+aaaa+"</li>";
-
-			//}
-			//for (int i = 0; i < liste.Count(); i++)
-			foreach (var y in liste)
-			{
-			list.InnerHtml += @"<div class=""item""> <img class=""ui avatar image"" src="""+y.Value+@"""/><div class=""content""> <div class=""header""> "+y.Key+"</div></div></div>";
-				
-			}
+			//	}
 			//string yalcin = "KADIN";
 			//if (yalcin == "ERKEK")
 			//{ Response.Write("doğru"); }
@@ -58,6 +56,15 @@ namespace ilkcalisma
 			////Response.Write((14 > myNum) ?"doğru" : "y");
 			//string txt = "<br/>We are the so-called \"Vikings\" from the north.";
 			//Response.Write(txt);
+			int x = 10;
+			int y = 20;
+			int z = x + y;
+			int a = z + 10;
+			int b = a + 10;
+
+			Response.Write(z);
+
+
 
 
 
